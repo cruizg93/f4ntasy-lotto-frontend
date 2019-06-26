b6ddc08400c638057bb95692b0f20954a4c8337b
Ale <alemolerio@gmail.com>
2019-06-26 14:30:55 -0400

    Fix Fijar issues with new auth service

--- src/components/Fijar/Fijar.js
+++ src/components/Fijar/Fijar.js
@@ -1,7 +1,6 @@
 import React, {useState, useEffect} from 'react';
 import {makeStyles} from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
-import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
 import List from '@material-ui/core/List';
 
--- src/service/api/fijar/fijar.js
+++ src/service/api/fijar/fijar.js
@@ -1,14 +1,11 @@
-import { baseUrl, tokenStr } from '../../../config/const';
+import {baseUrl, tokenStr} from '../../../config/const';
+import {authHeader} from "../../../_helpers/auth-header";
+import {authenticationService} from "../authentication/authentication.service";
+
 export function list_numeros() {
+    const requestOptions = {method: 'GET', headers: authHeader()};
     return new Promise((resolve, reject) => {
-        fetch(`${baseUrl}/admin/numeros/fijados`, {
-            method: 'GET',
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json',
-                "Authorization": `Bearer ${tokenStr}`
-            }
-        })
+        fetch(`${baseUrl}/admin/numeros/fijados`, requestOptions)
             .then((response) => response.json())
             .then((responseJson) => {
                 resolve(responseJson);
@@ -19,17 +16,20 @@ export function list_numeros() {
     });
 }
 
-export function update_number(updateNumber){
-     return new Promise((resolve, reject) => {
-        fetch(`${baseUrl}/admin/numeros/fijados/add`, {
-            method: 'POST',
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json',
-                "Authorization": `Bearer ${tokenStr}`
-            },
-            body: JSON.stringify(updateNumber)
-        })
+export function update_number(updateNumber) {
+    const currentUser = authenticationService.currentUserValue;
+    const requestOptions = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+            "Authorization": `Bearer ${currentUser.accessToken}`
+        },
+        body: JSON.stringify(updateNumber)
+    };
+    return new Promise((resolve, reject) => {
+        fetch(`${baseUrl}/admin/numeros/fijados/add`,
+            requestOptions)
             .then((response) => response.json())
             .then((responseJson) => {
                 resolve(responseJson);
